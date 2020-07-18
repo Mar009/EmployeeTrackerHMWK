c62e7ddb5da0969e1ef9ce3a911cc820d310c948
MMMarz009 <mar0099887@gmail.com>
2020-07-18 16:50:18 -0500

    Made basic index with notes for future ref

---
+++ index.js
@@ -0,0 +1,47 @@
+//Consts
+const connection = require("./db/connection.js");
+const db = require("./db");
+const inquirer = require("inquirer");
+require("console.table");
+
+//Display logo
+
+
+//options
+    //view dept
+    //view roles
+    //view employess
+    //add dept
+    //add roles
+    //add employee
+    //update employee role
+//exit;
+
+
+
+
+//Display dept
+
+
+//Display roles
+
+//display employees
+
+
+//add dept
+
+
+//add roles
+
+//add employee
+
+
+//update employee
+
+
+//exit
+
+
+
+
+
